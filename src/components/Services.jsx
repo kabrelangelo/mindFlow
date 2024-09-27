a95824deb8ed2c38bb3150516871fe63a9dd7235
unknown <ngoumkwa.kenne@gmail.com>
2024-09-27 14:12:24 +0100

    Services

--- src/components/Services.jsx
+++ src/components/Services.jsx
@@ -11,12 +11,37 @@ const Services = () => {
                 MindFlow débloque tout le potentiel de l'intelligence artificielle pour votre vie au quotidien
             </p>
         </div>
-        <div className=' grid lg:grid-cols-3 grid-cols-1 gap-5 border border-gray-500 rounded-xl p-5'>
+        <div className=' grid lg:grid-cols-3 grid-cols-1 gap-5 border border-gray-500 rounded-xl p-2'>
             <div className='lg:col-span-2 '>
-                <img src="./src/assets/services/service-1.png" alt="Service" className="w-full rounded-xl" />
+                <img src="./src/assets/services/service-1.png" alt="Service" className="w-full object-cover rounded-xl" />
             </div>
-            <div className=' flex justify-center items-center p-5 rounded-xl'>
-                <p>Contenu ici</p>
+            <div className='justify-center items-center lg:my-40 p-5 rounded-xl'>
+                <h4 className='lg:text-3xl text-xl m-5 font-bold'>La plus futée des IA</h4>
+                <p className='m-5 '>MindFlow débloque le potentiel de l'intelligence artificielle.</p>
+                <div className='m-5'>
+                <span className="flex items-center py-4">
+                    <span className="h-px flex-1 bg-black"></span>
+                </span>
+                <div className='flex'>
+                <img src="./src/assets/check.svg" alt=""  />
+                <h6 className='text-xl font-semibold mx-4'>Génération de photo</h6>
+                </div>
+                <span className="flex items-center py-4">
+                    <span className="h-px flex-1 bg-black"></span>
+                </span>
+                <div className='flex'>
+                <img src="./src/assets/check.svg" alt=""  />
+                <h6 className='text-xl font-semibold mx-4'>Amélioration de photo</h6>
+                </div>
+                <span className="flex items-center py-4">
+                    <span className="h-px flex-1 bg-black"></span>
+                </span>
+                <div className='flex'>
+                <img src="./src/assets/check.svg" alt=""  />
+                <h6 className='text-xl font-semibold mx-4'>Intégration transparente</h6>
+                </div>
+                
+                </div>
             </div>
         </div>
     </div>
